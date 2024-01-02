6f38c004848156593553fba16a3b4bbea59d3ebe
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-01 22:34:34 -0300

    tailwind config dark mode

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -93,7 +93,7 @@ export const MoonIcon = () => {
       viewBox="0 0 32 32"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
-      stroke="#ffffff"
+      stroke="#000000"
     >
       <g id="SVGRepo_bgCarrier" stroke-width="0" />
 
