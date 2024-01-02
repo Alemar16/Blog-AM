08f6434c5d1b465020414c4889e49828d779dce4
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-01 23:39:12 -0300

    Styles title

--- app/components/Nabvar.tsx
+++ app/components/Nabvar.tsx
@@ -1,13 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
 import ThemeSwitch from './ThemeSwitch'
+import {Lilita_One} from 'next/font/google'
+
+const lilitaOne = Lilita_One({ weight: '400', subsets: ['latin'] })
 
 const Nabvar = () => {
   return (
     <div className='mx-auto max-w-5xl px-6'>
         <div className='flex justify-between items-center h-16 w-full'>
             <Link href="/">
-                <div>Blog AM</div>
+                <div className={`${lilitaOne.className} text-3xl dark:text-amber-50`}>Blog
+                <span className='text-purple-500'>AM</span>
+                </div>
             </Link>
             <ThemeSwitch />
         </div>
