36f6434c4b2e816c6f08ae9b0ebd91ef28812ee3
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-01 22:22:28 -0300

    search icons dark-lithg mode from https://www.svgrepo.com/

---
+++ app/components/Icons.tsx
@@ -0,0 +1,112 @@
+export const SunIcon = () => {
+  return (
+    <svg
+      width="24px"
+      height="24px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#a955f7"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z"
+          stroke="#a955f7"
+          stroke-width="2"
+        />{" "}
+        <path
+          d="M12 5V3"
+          stroke="#a955f7"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />{" "}
+        <path
+          d="M17 7L19 5"
+          stroke="#a955f7"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />{" "}
+        <path
+          d="M19 12H21"
+          stroke="#a955f7"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />{" "}
+        <path
+          d="M17 17L19 19"
+          stroke="#a955f7"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />{" "}
+        <path
+          d="M12 19V21"
+          stroke="#a955f7"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />{" "}
+        <path
+          d="M7 17L5 19"
+          stroke="#a955f7"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />{" "}
+        <path
+          d="M5 12H3"
+          stroke="#a955f7"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />{" "}
+        <path
+          d="M5 5L7 7"
+          stroke="#a955f7"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />{" "}
+      </g>
+    </svg>
+  );
+};
+
+export const MoonIcon = () => {
+  return (
+    <svg
+      fill="#a955f7"
+      width="24px"
+      height="24px"
+      viewBox="0 0 32 32"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <title>moon</title>{" "}
+        <path d="M29.684 23.984l-0.021-0.055c-0.193-0.456-0.638-0.771-1.155-0.771-0.172 0-0.336 0.035-0.486 0.098l0.008-0.003c-1.219 0.521-2.638 0.824-4.127 0.824-5.937 0-10.751-4.813-10.751-10.751 0-4.458 2.713-8.282 6.579-9.911l0.071-0.026 0.221-0.093c0.444-0.199 0.747-0.636 0.747-1.145 0-0.625-0.458-1.142-1.057-1.235l-0.007-0.001c-0.676-0.106-1.455-0.167-2.249-0.167-8.421 0-15.248 6.827-15.248 15.248s6.827 15.248 15.248 15.248c4.91 0 9.278-2.321 12.067-5.925l0.026-0.035c0.161-0.208 0.258-0.473 0.258-0.76 0-0.197-0.046-0.383-0.127-0.548l0.003 0.007zM22.326 27.779c-1.44 0.612-3.115 0.968-4.873 0.968-7.041 0-12.75-5.708-12.75-12.75 0-6.189 4.41-11.349 10.26-12.507l0.081-0.013c-2.694 2.435-4.379 5.942-4.379 9.843 0 7.319 5.933 13.252 13.252 13.252 0.242 0 0.482-0.006 0.72-0.019l-0.033 0.001c-0.655 0.45-1.404 0.86-2.194 1.193l-0.084 0.031z" />{" "}
+      </g>
+    </svg>
+  );
+};
