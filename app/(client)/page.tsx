3694275e87e3fcb099bc50349cc8a4b9a1ae375a
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-06 00:40:04 -0300

    update file + correction function

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,40 +1,39 @@
-import {client} from '@/sanity/lib/client'
-import Header from '../components/Header';
-import { Post } from '../utils/interface';
-import PostComponent from '../components/PostComponent';
+import { client } from "@/sanity/lib/client";
+import Header from "../components/Header";
+import { Post } from "../utils/interface";
+import PostComponent from "../components/PostComponent";
 
 async function getPosts() {
-  const query =
-    `*[_type == "post"]{
-      title,
+  const query = `
+  *[_type == "post"] {
+    title,
+    slug,
+    publishedAt,
+    excerpt,
+    tags[]-> {
+      _id,
       slug,
-      publishedAt,
-      excerpt,
-      tags[]-> {
-        _id,
-        slug,
-        name
-      }
+      name
     }
-    `;
-    const data = await client.fetch(query)
-    return data
+  }
+  `;
+  const data = await client.fetch(query);
+  return data;
 }
 
-export const revalidate = 60
-export default async function Home() {
-  const posts: Post[] = await getPosts()
-  console.log(posts, 'posts')
-  return (
-<div>
-  <Header title='Articles' />
-  <div>
-    {posts?.length > 0 && posts?.map((post) => (
-     <PostComponent key={post?._id} post={post} />
-       
-     ))}
-      </div>
-  </div>
+export const revalidate = 60;
 
-  )
+export default async function Home() {
+  const posts: Post[] = await getPosts();
+  console.log(posts, "posts");
+
+  return (
+    <div>
+      <Header title="Articles" tags />
+      <div>
+        {posts?.length > 0 &&
+          posts?.map((post) => <PostComponent key={post?._id} post={post} />)}
+      </div>
+    </div>
+  );
 }
