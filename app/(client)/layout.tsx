42cdeb585bf0bfc65dd016e346e7f28935d3101d
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-06 00:36:29 -0300

    update files

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
-import { Inter, Fira_Code } from "next/font/google";
+import { Fira_Code } from "next/font/google";
 import "./globals.css";
-import Nabvar from "../components/Nabvar";
+import Navbar from "../components/Navbar";
 import { Provider } from "../utils/Provider";
 
-const inter = Inter({ subsets: ["latin"] });
 const firaCode = Fira_Code({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,10 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${firaCode.className} h-full bg-amber-50 text-indigo-950 dark:bg-slate-950 dark:text-amber-50 dark:selection:bg-purple-500`}>
+      <body
+        className={`${firaCode.className} h-full bg-amber-50 text-indigo-950 dark:bg-slate-950 dark:text-amber-50 dark:selection:bg-purple-500`}
+      >
         <Provider>
-          <Nabvar />
-          <main className="mx-auto max-w-5xl px-6">{children}</main>
+          <Navbar />
+          <main className="h-full mx-auto max-w-5xl px-6">{children}</main>
         </Provider>
       </body>
     </html>
