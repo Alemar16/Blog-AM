d29713f79b451291094214150dd2da8bb436e623
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-05 23:16:51 -0300

    variable correction in camecase

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -8,12 +8,12 @@ export const SunIcon = () => {
       xmlns="http://www.w3.org/2000/svg"
       stroke="#fff8e1 "
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+      <g id="SVGRepo_bgCarrier" strokeWidth="0" />
 
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
 
       <g id="SVGRepo_iconCarrier">
@@ -21,63 +21,63 @@ export const SunIcon = () => {
         <path
           d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z"
           stroke="#fff8e1 "
-          stroke-width="2"
+          strokeWidth="2"
         />{" "}
         <path
           d="M12 5V3"
           stroke="#fff8e1 "
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />{" "}
         <path
           d="M17 7L19 5"
           stroke="#fff8e1 "
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />{" "}
         <path
           d="M19 12H21"
           stroke="#fff8e1 "
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />{" "}
         <path
           d="M17 17L19 19"
           stroke="#fff8e1 "
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />{" "}
         <path
           d="M12 19V21"
           stroke="#fff8e1 "
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />{" "}
         <path
           d="M7 17L5 19"
           stroke="#fff8e1 "
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />{" "}
         <path
           d="M5 12H3"
           stroke="#fff8e1 "
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />{" "}
         <path
           d="M5 5L7 7"
           stroke="#fff8e1 "
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />{" "}
       </g>
     </svg>
@@ -95,12 +95,12 @@ export const MoonIcon = () => {
       xmlns="http://www.w3.org/2000/svg"
       stroke="#a955f7 "
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+      <g id="SVGRepo_bgCarrier" strokeWidth="0" />
 
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
 
       <g id="SVGRepo_iconCarrier">
@@ -123,12 +123,12 @@ export const BackArrowIcon = () => {
       height="24px"
       viewBox="0 0 45.58 45.58"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+      <g id="SVGRepo_bgCarrier" strokeWidth="0" />
 
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
 
       <g id="SVGRepo_iconCarrier">
